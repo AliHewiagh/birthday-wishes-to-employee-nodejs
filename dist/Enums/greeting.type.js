374ddf39d098bf7e3dc4dabba675837381101615
Alihewaigh <alihewaigh@gmail.com>
2021-06-26 01:26:14 +0800

    remove gitignore files

--- dist/Enums/greeting.type.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var GreetingType;
-(function (GreetingType) {
-    GreetingType["BIRTHDAY"] = "BIRTHDAY";
-})(GreetingType || (GreetingType = {}));
-exports.default = GreetingType;
